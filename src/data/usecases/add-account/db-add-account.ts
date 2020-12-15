33977dad74dae5601eb6612a29bc0b28d99e6722
Tarcisio Costa <tarcisiocorte@gmail.com>
2020-12-15 10:36:08 -0300

    chore: update all files

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,14 @@
+import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return new Promise(resolve => resolve(null))
+  }
+}
