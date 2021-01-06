809e64c9cf94c9e7e4bc24108b1d7b21e844d43c
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-01-05 23:50:12 -0300

    feat: update code and add new middleware

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Rodrigo',
+        email: 'rodrigo.manguinho@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
