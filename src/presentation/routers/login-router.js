1100fb0ae09a36499c4ed4406d63b90db278ce70
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-05-27 18:53:35 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,25 +1,30 @@
-const HttpResponse = require('../helpers/http-response')
+const HttpResponse = require("../helpers/http-response");
 
 module.exports = class LoginRouter {
-  constructor (authUseCase) {
-    this.authUseCase = authUseCase
+  constructor(authUseCase) {
+    this.authUseCase = authUseCase;
   }
 
-  route (httpRequest) {
-    if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
-      return HttpResponse.serverError()
+  route(httpRequest) {
+    if (
+      !httpRequest ||
+      !httpRequest.body ||
+      !this.authUseCase ||
+      !this.authUseCase.auth
+    ) {
+      return HttpResponse.serverError();
     }
-    const { email, password } = httpRequest.body
+    const { email, password } = httpRequest.body;
     if (!email) {
-      return HttpResponse.badRequest('email')
+      return HttpResponse.badRequest("email");
     }
     if (!password) {
-      return HttpResponse.badRequest('password')
+      return HttpResponse.badRequest("password");
     }
-    const accessToken = this.authUseCase.auth(email, password)
+    const accessToken = this.authUseCase.auth(email, password);
     if (!accessToken) {
-      return HttpResponse.unauthorizedError()
+      return HttpResponse.unauthorizedError();
     }
-    return HttpResponse.ok()
+    return HttpResponse.ok({ accessToken });
   }
-}
+};
